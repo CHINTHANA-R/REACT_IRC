dda79974f808151281dfeabe4d3f726e0fd36db1
CHINTHANA R <153272208+CHINTHANA-R@users.noreply.github.com>
2023-12-14 12:36:14 +0530

    Add files via upload

---
+++ Toy Store/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import LoginSignup from './components/Loginsignup/lLoginSignup';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
